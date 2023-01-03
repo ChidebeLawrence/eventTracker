3582f4dfa5383f7fae25e86fb6c10f9e7a642e32
Chidebe Lawrence <lawrencechidebe@Gmail.com>
2023-01-03 14:48:43 +0100

    created a header, left nav and section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
+import logo from './assets/images/logo.png'
+import profile_img from './assets/images/profile_img.png'
+import { BiChevronDown } from 'react-icons/bi'
+import { IoChevronBackOutline } from 'react-icons/io5'
+
 function App() {
-  return <h1 className="bg-yellow-500 text-xl">hello world</h1>
+  return (
+    <div className="text-white">
+      <header className="bg-seagreen px-3 py-2 border-none flex justify-between">
+        <div className="flex justify-between items-center gap-5">
+          <img src={logo} alt="logo" className="h-10 w-10 object-cover" />
+          <h1>Zazu Events</h1>
+        </div>
+        <div className="flex justify-between items-center gap-10">
+          <p className="flex items-center gap-2 text-xs">
+            Help <BiChevronDown />
+          </p>
+          <img
+            src={profile_img}
+            alt="profile_img"
+            className="h-10 w-10 rounded-full object-cover"
+          />
+        </div>
+      </header>
+
+      <div className="flex items-center">
+        <nav className="bg-seagreen w-1/5 h-screen p-3 text-xs">
+          <div className="flex items-center gap-2 cursor-pointer hover:underline">
+            <IoChevronBackOutline />
+            Back to list
+          </div>
+        </nav>
+        <section className="bg-feece8 h-screen w-full"></section>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      seagreen: '#0c3934',
+      white: 'white',
+      feece8: '#feece8',
+    },
   },
   plugins: [],
 }
